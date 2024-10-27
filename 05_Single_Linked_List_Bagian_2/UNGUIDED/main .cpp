90cfedf8998a682452258f1e9fe866fc6bc0b5a1
nopitaa <novitasyahwahapsari@gmail.com>
2024-10-27 12:53:14 +0700

    modul 5 struktur data

--- 05_Single_Linked_List_Bagian_2/UNGUIDED/main .cpp
+++ 05_Single_Linked_List_Bagian_2/UNGUIDED/main .cpp
@@ -10,16 +10,12 @@ int main()
     // alokasi + insert data
     P1 = alokasi(2);
     insertFirst(L, P1);
-
     P2 = alokasi(0);
     insertFirst(L, P2);
-
     P3 = alokasi(8);
     insertFirst(L, P3);
-
     P4 = alokasi(12);
     insertFirst(L, P4);
-
     P5 = alokasi(9);
     insertFirst(L, P5);
 
